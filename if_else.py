5232d8d1cfa50565b7c050f9104e681cce428f6e
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-18 09:32:17 -0300

    Adicionado o arquivo if_else.py

---
+++ if_else.py
@@ -0,0 +1,7 @@
+# Uso do if e else
+condiçao = 200
+numero = int(input('Digite o número: '))
+if numero == condiçao:
+    print(f'Este é o número correto! {numero} ')
+else: 
+    print(f'O número digitado está errado. {numero} ')
