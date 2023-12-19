bf6fd574cb058315d2d8db512bdc1dec89d43707
Venicius Vieira <veniciusvieira90@gmail.com>
2023-12-19 13:45:43 -0300

    Adicionados os arquivos atv01, atv02, bomba_gasolina e deletado teste3

---
+++ strings/atv01.py
@@ -0,0 +1,5 @@
+# Utilize o método split para separar a string "Olá, Mundo!" em uma lista de palavras.
+
+string = "Olá, Mundo!"
+lista_string = string.split()
+print(lista_string)
---
+++ strings/atv02.py
@@ -0,0 +1,6 @@
+# Crie duas variáveis contendo as strings "Python" e "é divertido!". Concatene-as para formar uma nova string.
+
+string = "Python"
+string2 = "é divertido!"
+nova_string = string + " " + string2
+print(nova_string)
